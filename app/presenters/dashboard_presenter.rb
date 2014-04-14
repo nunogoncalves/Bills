dffa0e6da4221e489f49a8a696b2ed787fb1d78d
Nuno Gonçalves <nuno.goncalves@linkedcare.com>
2014-04-14 08:38:43 +0100

    prettier.. a bit.

--- app/presenters/dashboard_presenter.rb
+++ app/presenters/dashboard_presenter.rb
@@ -1,5 +1,30 @@
 class DashboardPresenter < Presenter
 
+	TENANTS_LIST = %w(SamuelLJackson  JohnMalkovich  BradPitt  RichardGere  TobeyMaguire  VinDiesel  MartinLawrence  JohnTravolta  JimCarrey
+		  JohnGoodman  AdamSandler  AlfredMolina  MarkWahlberg ArnoldSchwarzenegger  EdwardNorton  DanielRadCliffe  LukeWilson  
+			MeganFox  AlecBaldwin  MerylStreep  JohnCusack  EmilyBlunt  CharlizeTheron  RobertDuvall  JonVoight 
+			AndyGarcia  BruceWillis  TomHanks  DennisQuaid  GeorgeClooney  KirstenDunst  AnnaFaris  AmandaPeet  EddieMurphy
+			GwynethPaltrow  DonCheadle  RobinWilliams  TomCruise  AlPacino  PaulBettany  KateBeckinsale  PenelopeCruz  MegRyan  
+			NicoleKidman EdHarris  TommyLeeJones  DustinHoffman  RachelMcAdams  DanielDay-Lewis JenniferLoveHewitt  AndieMacDowell
+		  SandraBullock  ReeseWitherspoon  BillyBobThorton  JessicaBiel  SigourneyWeaver  HalleBerry  JessicaAlba  
+		  ElizabethBanks  BillyCrystalBryceDallasHoward  SteveMartin  ChristianBale  JeremyIrons  CameronDiaz  KevinSpacey
+			KevinKostner  BillMurray  MichaelDouglas  LiamNeeson  JohnnyDepp  ScarlettJohansson  KateWinslet  OliviaWilde
+			DanielCraig  RobSchneider  angelinaJolie  EmmaStone  EmmaThompson  TedDanson  JamesGandolfini  CaseyAffleck
+			HeatherLocklear  AnnaKendrick  ReneeZellweger  TomWilkinson  KevinKline  MaggieGrace  KatieHolmes
+			SteveMcQueen  LeonardoDiCaprio  JeffBridges  KirstieAlley  WillFerrell  HilaryDuff  DrewBarrymore  GaryOldman
+			EmbethDavidtz  LeslieMann  PaulNewman  EvangelineLilly  NaomiWatts JasonBiggs  MenaSuvari  JasonStatham  GlennClose
+			SalmaHayek  DonaldSutherland  CateBlanchett  RalphFiennes  KeiraKnightley  JamesStewart  WoodyHarrelson DannyDeVito  
+			RussellCrowe  PaulWalker  HeathLedger  ClintEastwood  SylvesterStallone  MorganFreeman  AmyAdams  AnthonyHopkins  WillSmith  AliciaSilverstone
+			MattDamon  SethGreen  SamNeil  PaulDano  SteveZahn  ChristopherPlummer  MickeyRourke  SusanSarandon  TimothyOlyphant  RainnWilson  NicolasCage
+			JamesFranco  HarrisonFord  JakeGyllenhaal  JudeLaw  TimMeadows  DemiMoore  CatherineKeener  BenAffleck  AnneHathaway  MelGibson  JackNicholson  MarisaTomei  EvaMendes
+			RayLiotta  JuliaRoberts  ChristopherWalken  VinceVaughn  CliveOwen  JenniferGarner  BridgetteWilson-Sampras  SteveCarell  SarahMichelleGellar  CatherineZeta-
+			Jones  JamesRemar  JackBlack  ClaireForlani  StanleyTucci  RyanReynolds  JamieLeeCurtis  EwanMcGregor  BenStiller  ChristinaRicci  RachelWeisz  RowanAtkinson
+			PaulGiamatti  LuisGuzman  EvaLongoria  ElizaDushku  EmileHirsch  MichaelCaine  JamesWoods  ClaireDanes  SeanPenn  AlanArkin  WillemDafoe
+			JodieFoster  ToniCollette  MargHelgenberger  MichellePfeiffer  BlakeLively  KeanuReeves  JenniferConnelly  ValKilmer  GeneHackman
+			RobertRedford  AntonioBanderas  HankAzaria  AshleyJudd  KristinScottThomas  OwenWilson  QueenLatifah  StellanSkarsgard  ElishaCuthbert
+			JeanClaudeVanDamme  TeaLeoni  JackieChan  JenniferAniston  JosephGordonLevitt  DenzelWashington  ColinFarrell  MandyMoore
+			CarrieAnneMoss  CubaGoodingJr)
+
 	def tenants
 		@tenants ||= Tenant.all.map { |tenant| TenantPresenter.new(h, tenant)}
 	end
@@ -9,7 +34,11 @@ class DashboardPresenter < Presenter
 	end
 
 	def possible_bills
-		@list ||= {"water" => "#2f7ed8", "gas" => "#CC0000", "power" => "#8bbc21"}
+		@list ||= {"water" => "#03BAF6", "gas" => "#CC0000", "power" => "#8bbc21"}
+	end
+
+	def tenant_sample_image
+		"https://dl.dropboxusercontent.com/u/2001692/actor/#{TENANTS_LIST.sample}.jpg"
 	end
 
 end
